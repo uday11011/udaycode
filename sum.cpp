aae752b2c335326f586b457a325dde769992bd0f
uday kiran niddena <udaykiran.11011@gmail.com>
2022-04-05 18:53:05 +0530

    initial comment

---
+++ sum.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+ using namespace std;
+
+ int main()
+ {
+
+     
+     int a=10, b=20;
+
+     cout<<"the given numbers are :"<<a<<endl<<b<<endl;
+     cout<<"addition of two numbers is :"<<a+b<<endl;
+     cout<< "added number is : "<<a+b;
+
+     return 0;
+
+ }
